e147d820e86b17f4fafccac77a0d70e48ac727c7
Harsh <harshthite593@gmail.com>
2026-04-15 23:41:36 +0530

    first commit

---
+++ Day11/patterns.java
@@ -0,0 +1,13 @@
+public class patterns {
+    public static void main(String[] args) {
+int n = 4;
+        for (int i = 0; i < 2*n - 1; i++) {
+
+            for (int j = n; j < ; j++) {
+
+            }
+
+            System.out.println(); // move to next line after each row
+        }
+    }
+}
