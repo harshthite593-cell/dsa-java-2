1e68c79a25992240796ea9fd33d05676a790ab14
Harsh <harshthite593@gmail.com>
2026-04-15 23:46:38 +0530

    updated

--- Day11/patterns.java
+++ Day11/patterns.java
@@ -1,13 +1,38 @@
 public class patterns {
     public static void main(String[] args) {
-int n = 4;
-        for (int i = 0; i < 2*n - 1; i++) {
 
-            for (int j = n; j < ; j++) {
+        int n = 5;
 
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
 
-            System.out.println(); // move to next line after each row
+            // stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
         }
     }
 }
