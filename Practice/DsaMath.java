3143a519018e884779053de921f4531252df848c
Harsh <harshthite593@gmail.com>
2026-04-19 00:29:10 +0530

    new

---
+++ Practice/DsaMath.java
@@ -0,0 +1,15 @@
+public class DsaMath {
+    static void main(String[] args) {
+        int n = 6 ;
+        int last  = 0 ;
+        int base = 5 ;
+        int ans = 0;
+        while(n > 0){
+            last = n & 1;
+            n  = n>>1;
+            ans += base*last;
+            base = base * 5;
+        }
+        System.out.println(ans);
+    }
+}
