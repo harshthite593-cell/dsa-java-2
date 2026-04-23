5302ab7b6f2396f65b47de414e57cc986aae6eb5
Harsh <harshthite593@gmail.com>
2026-04-23 20:55:41 +0530

    new

--- Day18/Recersion2ndlec.java
+++ Day18/Recersion2ndlec.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class Recersion2ndlec {
-    static void main(String[] args) {
-        System.out.println("hellow ");
+    public static void main(String[] args) {
+        int[] arr = {1,4,5,7,3,8,9};
+        sort(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr, int r, int c, int max) {
+        if (r == 0) {
+            return;
+        }
+        if( c < r) {
+            if (arr[c] > arr[max]) {
+                sort(arr, r, c + 1, c);
+            } else {
+                sort(arr, r, c + 1, max);
+            }
+        }else{
+            int temp = arr[c];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            sort(arr,r - 1,0,0);
+            }
+
     }
 }
