6d12ad8f069424374a75e4b7fb736fabb6732d81
Harsh <harshthite593@gmail.com>
2026-04-29 17:43:37 +0530

    new

--- Day25/solution3.java
+++ Day25/solution3.java
@@ -1,2 +1,32 @@
-public class solution3 {
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        int count = -1;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                ListNode pos = head;
+                while (pos != slow){
+                    pos = pos.next;
+                    slow = slow.next;
+                }
+                return pos;
+            }
+        }
+        return null;
+    }
 }
