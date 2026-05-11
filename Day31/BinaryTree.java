69ce49a7dfb4f37fa3feb179246307a6b7b08ce4
Harsh <harshthite593@gmail.com>
2026-05-11 21:35:54 +0530

    new

--- Day31/BinaryTree.java
+++ Day31/BinaryTree.java
@@ -1,2 +1,54 @@
+import java.util.*;
+
 public class BinaryTree {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+    // Build tree recursively
+    public Node buildTree() {
+        System.out.print("Enter value (-1 for no node): ");
+        int val = sc.nextInt();
+
+        if (val == -1) {
+            return null;
+        }
+
+        Node root = new Node(val);
+
+        System.out.println("Enter left of " + val);
+        root.left = buildTree();
+
+        System.out.println("Enter right of " + val);
+        root.right = buildTree();
+
+        return root;
+    }
+
+    // Inorder traversal
+    public void inorder(Node root) {
+        if (root == null) return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+
+        System.out.println("Build your tree:");
+        Node root = bt.buildTree();
+
+        System.out.println("\nInorder traversal:");
+        bt.inorder(root);
+    }
 }
--- Practice/practice.java
+++ Practice/practice.java
@@ -1,32 +1,32 @@
-class Solution {
-    public int minAddToMakeValid(String s) {
-
-        String word;
-        word = s;
-        int square = 0;
-        int curly = 0;
-        int circle =0;
-        int sum = 0;
-        for ( int i  = 0 ; i < word.length() ; i++){
-            if (word.charAt(i) == '{'){
-                curly++;
-            }
-            if ( word.charAt(i) == '[' {
-                square++;
-            }
-            if ( word.charAt(i) == '('){
-                circle++;
-            }
-            if ( word.charAt(i) == '}'){
-                curly--;
-            }
-            if ( word.charAt(i) == ']'){
-                square--;
-            }
-            if ( word.charAt(i) == ')'){
-                circle--;
-            }
-            }
-        return Math.abs(circle) + Math.abs(curly) + Math.abs(square);
-        }
-
+//class Solution {
+//    public int minAddToMakeValid(String s) {
+//
+//        String word;
+//        word = s;
+//        int square = 0;
+//        int curly = 0;
+//        int circle =0;
+//        int sum = 0;
+//        for ( int i  = 0 ; i < word.length() ; i++){
+//            if (word.charAt(i) == '{'){
+//                curly++;
+//            }
+//            if ( word.charAt(i) == '[') {
+//                square++;
+//            }
+//            if ( word.charAt(i) == '('){
+//                circle++;
+//            }
+//            if ( word.charAt(i) == '}'){
+//                curly--;
+//            }
+//            if ( word.charAt(i) == ']'){
+//                square--;
+//            }
+//            if ( word.charAt(i) == ')'){
+//                circle--;
+//            }
+//            }
+//        return Math.abs(circle) + Math.abs(curly) + Math.abs(square);
+//        }
+//
