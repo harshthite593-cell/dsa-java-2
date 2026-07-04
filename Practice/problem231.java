5c3551a0a150a2a696cd2349af0b5807231e149c
Harsh <harshthite593@gmail.com>
2026-07-05 00:23:13 +0530

    new

--- Practice/problem231.java
+++ Practice/problem231.java
@@ -1,2 +1,19 @@
-public class problem231 {
-}
+//public class problem231 {
+//    static void main(String[] args) {
+//        int n = 32;
+//        int m = 1;
+//        if(n>11){
+//            if((n/10)%2==0){
+//                n=n/10;
+//            }
+//        }
+//        while (m <= n) {
+//            if(m==n){
+//                System.out.println("true");
+//            }
+//            m*=2;
+//            System.out.println(m);
+//        }
+//        System.out.println("false");
+//    }
+//}
